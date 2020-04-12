3534bdb944ca8476715a07e345810ec86e683dbe
loic-prosi <loicprosi@gmail.com>
2020-04-12 18:29:51 +0200

    <Feature> Added test component

---
+++ src/components/CustomButton/CustomButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./CustomButton.css";
+import "./CustomButton.less";
+
+const CustomButton = (props) => {
+  return <button className="css-class less-class">Button</button>;
+};
+
+export default CustomButton;
---
+++ src/components/CustomButton/CustomButton.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CustomButton from "./CustomButton";
+
+export default {
+  title: "Button",
+  component: CustomButton,
+};
+
+export const Button = () => <CustomButton />;
